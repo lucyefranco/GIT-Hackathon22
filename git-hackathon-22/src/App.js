2d1356381ad732c7c9981dbb3368e76e40ae288f
LFranco25 <Lucia.Franco25@t-mobile.com>
2022-10-15 16:54:54 -0700

    add routing

--- git-hackathon-22/src/App.js
+++ git-hackathon-22/src/App.js
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Pages/Home';
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return(
+    <Router>
+        <Routes>
+          <Route exact path="/" element={Home} />
+        </Routes>
+    </Router>
+  ) 
 }
 
 export default App;
