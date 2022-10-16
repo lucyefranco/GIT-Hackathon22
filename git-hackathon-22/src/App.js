8a3e768c1efa2b2d2f211f19ec8925e24b734be7
Megan Donaldson <meganmaried123@gmail.com>
2022-10-15 23:29:41 -0500

    added the get help page

--- git-hackathon-22/src/App.js
+++ git-hackathon-22/src/App.js
@@ -1,21 +1,19 @@
-import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from './Pages/Home';
-import SignIn from './Pages/SignIn'
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import HelpPage from "./Pages/HelpPage";
 
 function App() {
-  return(
+  return (
     <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="signin" element={<SignIn />}/>
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="signin" element={<SignIn />} />
+        <Route exact path="helppage" element={<HelpPage />} />
+      </Routes>
     </Router>
-  ) 
+  );
 }
 
 export default App;
