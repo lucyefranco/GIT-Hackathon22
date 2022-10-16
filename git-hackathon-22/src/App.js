0faf1ac2d18389d115b0011ec460e5bbecd10478
LFranco25 <Lucia.Franco25@t-mobile.com>
2022-10-15 21:33:38 -0700

    adds font and small styling

--- git-hackathon-22/src/App.js
+++ git-hackathon-22/src/App.js
@@ -4,17 +4,29 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn'
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Arvo',
+    ].join(','),
+  },
+});
+
+
 function App() {
   return(
+    <ThemeProvider theme={theme}>
     <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="signin" element={<SignIn />}/>
         </Routes>
     </Router>
+    </ThemeProvider>
   ) 
 }
 
--- git-hackathon-22/src/Pages/SignIn.js
+++ git-hackathon-22/src/Pages/SignIn.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Box, TextField, Container, Stack } from "@mui/material";
+import { Button, Box, TextField, Container, Stack, Divider } from "@mui/material";
 import { navigate, useNavigate } from "react-router-dom";
 import { Typography } from "@mui/material";
 import { display } from "@mui/system";
@@ -14,23 +14,30 @@ function SignIn() {
 
   return (
     <Container>
-        <Box align="center">
+        <Box align="center" marginTop={'30px'}>
         <img src={photo} alt="" style={photoStyling} />
 
         </Box>
         
 
       <Box display={"flex"} flexDirection={"column"}>
-        <Typography align="center">Sign In</Typography>
-        <TextField label="E-mail address" variant="outlined" />
+        <Typography sx={{fontWeight:'bold', color: '072E3E'}} align="center">Sign In</Typography>
+        <TextField padding={1} label="E-mail address" variant="outlined" />
         <TextField label="Password" variant="outlined" />
         <Typography>Forgot your password?</Typography>
         <Stack spacing={2} padding={2} marginTop={2}>
-            <Button variant="contained">Log In</Button>
+            <Button sx={{color:'009CDC', fontWeight:'bold'}} variant="contained">Log In</Button>
             <Typography align="center">OR</Typography>
             <Button variant="contained">Continue with Facebook</Button>
             <Button variant="contained">Continue with Google</Button>
         </Stack>
+        <Divider />
+        <Button onClick={navigate("/")}> Use as a guest </Button>
+        <Box display="flex">
+            <Typography> Not on our app?</Typography>
+            <Button>Sign up here</Button>
+        </Box>
+        
       </Box>
     </Container>
   );
